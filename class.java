ce4da0c686a66d4c259b9a1a13c11ba7d25c7cdf
Kozák <Martin.Kozak@telekom.com>
2024-10-23 10:52:16 +0200

    first commit

---
+++ class.java
@@ -0,0 +1,9 @@
+public class SimpleAdder {
+  public int add(int a, int b) {
+    return a + b;
+  }
+  public static void main(String[] args) {
+    SimpleAdder adder = new SimpleAdder();
+    System.out.println("Test: " + (adder.add(3, 4) == 7));
+  }
+}
